ee9ddde0b2aff86f62b372edd84d9621e57106a5
OgutuJoseph <onyango.joseph@yahoo.com>
2023-08-22 14:42:48 +0300

    Render items on Core App

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+# import models for displaying objects where applicable on frontend app
+from item.models import Category, Item
 
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=0)[0:6]
+    categories = Category.objects.all()
+    # return render(request, 'core/index.html')
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')

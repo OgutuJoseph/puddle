35060d14380fd8af9180cbfcd1f7dedc3916c194
OgutuJoseph <onyango.joseph@yahoo.com>
2023-08-22 10:04:27 +0300

    Creating model Category

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
+# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        # ordering = ('id',)
+        # ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name

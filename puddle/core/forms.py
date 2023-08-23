bac59c323464a6ed9a904f52b5098212dcb789a2
OgutuJoseph <onyango.joseph@yahoo.com>
2023-08-23 10:17:50 +0300

    Signing up

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -3,6 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
+    password1 = forms.CharField(
+        # label='Password',
+        widget=forms.PasswordInput(attrs={'class':'w-full py-4 px-6 rounded-xl', 'type':'password', 'placeholder':'Enter Password'})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class':'w-full py-4 px-6 rounded-xl', 'type':'password', 'placeholder':'Repeat Password'})
+    )
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
@@ -15,14 +23,15 @@ class SignupForm(UserCreationForm):
                 'placeholder': 'Enter Email',
                 'class': 'w-full py-4 px-6 rounded-xl'
             }),
-            'password1': forms.TextInput(attrs={
-                'placeholder': 'Enter Password',
-                'class': 'w-full py-4 px-6 rounded-xl'
-            }),
-            'password2': forms.PasswordInput(attrs={
-                'placeholder': 'Repeat Password',
-                'class': 'w-full py-4 px-6 rounded-xl'
-            })
+            # !!NOT WORKING -- USE Above ^^ Implementation
+            # 'password1': forms.PasswordInput(attrs={
+            #     'placeholder': 'Enter Email',
+            #     'class': 'w-full py-4 px-6 rounded-xl'
+            # }),
+            # 'password2': forms.PasswordInput(attrs={
+            #     'placeholder': 'Repeat Password',
+            #     'class': 'w-full py-4 px-6 rounded-xl'
+            # })
         }
 
         # !!NOT WORKING!!
